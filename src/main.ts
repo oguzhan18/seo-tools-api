df6a75ab40766943a4baec7ee78d786e910c77e3
oguzhan18 <oguzhancart1@gmail.com>
2024-07-29 19:57:33 +0300

    vercel deployment for added files

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,36 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+import { NestExpressApplication } from "@nestjs/platform-express";
+import { join } from "path";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   const config = new DocumentBuilder()
-    .setTitle('SEO TOOLS API')
-    .setDescription('All seo checking tool API of a particular website.')
-    .setVersion('1.0')
-    .addTag('seo')
+    .setTitle("SEO TOOLS API")
+    .setDescription("All seo checking tool API of a particular website.")
+    .setVersion("1.0")
+    .addTag("seo")
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup("api", app, document, {
+    customSiteTitle: "SEO TOOLS API",
+    customfavIcon: "https://avatars.githubusercontent.com/u/54273323?v=4",
+    customJs: [
+      "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.min.js",
+      "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.js"
+    ],
+    customCssUrl: [
+      "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css",
+      "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.css",
+      "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.css"
+    ]
+  });
+
+  app.useStaticAssets(join(__dirname, "..", "uploads"));
 
   await app.listen(3000);
 }
+
 bootstrap();
